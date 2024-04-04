e395e6e2fce604f87c89c53c26440214c8c0daec
xuhiang <chenshuotian2010@163.com>
2024-04-04 21:51:44 +0800

    first commit

---
+++ js/jieqi.js
@@ -0,0 +1,96 @@
+function lichun(){
+    let lichun1 = document.getElementById('lichun')
+    lichun1.style.display = 'block'
+}
+function yushui(){
+    let yushui1 = document.getElementById('yushui')
+    yushui1.style.display = 'block'
+}
+function jingzhe(){
+    let jingzhe1 = document.getElementById('jingzhe')
+    jingzhe1.style.display = 'block'
+}
+function chunfen(){
+    let chunfen1 = document.getElementById('chunfen')
+    chunfen1.style.display = 'block'
+}
+function qingming(){
+    let qingming1 = document.getElementById('qingming')
+    qingming1.style.display = 'block'
+}
+function guyu(){
+    let guyu1 = document.getElementById('guyu')
+    guyu1.style.display = 'block'
+}
+function lixia(){
+    let lixia1 = document.getElementById('lixia')
+    lixia1.style.display = 'block'
+}
+function xiaoman(){
+    let xiaoman1 = document.getElementById('xiaoman')
+    xiaoman1.style.display = 'block'
+}
+function mangzhong(){
+    let mangzhong1 = document.getElementById('mangzhong')
+    mangzhong1.style.display = 'block'
+}
+function xiazhi(){
+    let xiazhi1 = document.getElementById('xiazhi')
+    xiazhi1.style.display = 'block'
+}
+function xiaoshu(){
+    let xiaoshu1 = document.getElementById('xiaoshu')
+    xiaoshu1.style.display = 'block'
+}
+function dashu(){
+    let dashu1 = document.getElementById('dashu')
+    dashu1.style.display = 'block'
+}
+function liqiu(){
+    let liqiu1 = document.getElementById('liqiu')
+    liqiu1.style.display = 'block'
+}
+function chushu(){
+    let chushu1 = document.getElementById('chushu')
+    chushu1.style.display = 'block'
+}
+function bailu(){
+    let bailu1 = document.getElementById('bailu')
+    bailu1.style.display = 'block'
+}
+function qiufen(){
+    let qiufen1 = document.getElementById('qiufen')
+    qiufen1.style.display = 'block'
+}
+function hanlu(){
+    let hanlu1 = document.getElementById('hanlu')
+    hanlu1.style.display = 'block'
+}
+function shuangjiang(){
+    let shuangjiang1 = document.getElementById('shuangjiang')
+    shuangjiang1.style.display = 'block'
+}
+function lidong(){
+    let lidong1 = document.getElementById('lidong')
+    lidong1.style.display = 'block'
+}
+function xiaoxue(){
+    let xiaoxue1 = document.getElementById('xiaoxue')
+    xiaoxue1.style.display = 'block'
+}
+function daxue(){
+    let daxue1 = document.getElementById('daxue')
+    daxue1.style.display = 'block'
+}
+function dongzhi(){
+    let dongzhi1 = document.getElementById('dongzhi')
+    dongzhi1.style.display = 'block'
+}
+function xiaohan(){
+    let xiaohan1 = document.getElementById('xiaohan')
+    xiaohan1.style.display = 'block'
+}
+function dahan(){
+    let dahan1 = document.getElementById('dahan')
+    dahan1.style.display = 'block'
+}
